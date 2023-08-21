a930a31c68d86662338b1122eee6f737b2044834
kgpksh <tkdgns543@gmail.com>
2023-08-21 21:27:44 +0900

    feat : 게시물 리스트 구현

--- src/main/java/com/ksh/exam/springpractice1/app/article/controller/ArticleController.java
+++ src/main/java/com/ksh/exam/springpractice1/app/article/controller/ArticleController.java
@@ -1,14 +1,28 @@
 package com.ksh.exam.springpractice1.app.article.controller;
 
+import com.ksh.exam.springpractice1.app.article.dto.Article;
+import com.ksh.exam.springpractice1.app.article.service.ArticleService.ArticleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/article")
+@RequiredArgsConstructor
+@Slf4j
 public class ArticleController {
+    private final ArticleService articleService;
     @GetMapping("/list")
-    public String showList() {
+    public String showList(Model model) {
+        List<Article> articles = articleService.getArticles();
+
+        log.debug("articles : " + articles);
+        model.addAttribute("articles", articles);
         return "article/list";
     }
 
